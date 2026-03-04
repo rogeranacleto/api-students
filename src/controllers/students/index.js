b29fd7072c97e7b6d37cf833a000e5722543d52f
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-03-03 23:23:49 -0300

    chore: update controllers index exports

--- src/controllers/students/index.js
+++ src/controllers/students/index.js
@@ -1,4 +1,7 @@
 import listAllStudents from "./list-all.controller.js";
 import getStudentsById from "./get-by-id.controller.js";
+import createStudent from "./create-student.controller.js";
+import updateStudent from "./update-student.controller.js";
+import deleteStudent from "./delete-student.controller.js";
 
-export { listAllStudents, getStudentsById }
+export { listAllStudents, getStudentsById, createStudent, updateStudent, deleteStudent }
